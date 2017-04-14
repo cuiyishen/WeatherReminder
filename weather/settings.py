c48c6abe3e2707fc100948391439886e38971265
cuiyishen <cuiyishen@gmail.com>
2017-04-13 23:52:54 -0700

    2v

--- weather/settings.py
+++ weather/settings.py
@@ -38,7 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reminder',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'django.contrib.sites',
 ]
+
+SITE_ID = 1
+# auth and allauth settings
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+# Make email verification mandatory to avoid junk email accounts
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,6 +69,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
            os.path.join(BASE_DIR, 'templates', 'reminder'),
+           os.path.join(BASE_DIR, 'templates', 'allauth'),
            ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -125,3 +138,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import json
+email_config=dict()
+with open(os.path.join(BASE_DIR, 'weather', 'email_config.json')) as json_file:
+   email_config = json.load(json_file)
+EMAIL_USE_TLS = email_config['EMAIL_USE_TLS']
+EMAIL_HOST = str(email_config['EMAIL_HOST'])
+EMAIL_PORT = email_config['EMAIL_PORT']
+EMAIL_HOST_USER = str(email_config['EMAIL_HOST_USER'])
+EMAIL_HOST_PASSWORD = str(email_config['EMAIL_HOST_PASSWORD'])
+DEFAULT_FROM_EMAIL = str(email_config['DEFAULT_FROM_EMAIL'])
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend"
+)
+
